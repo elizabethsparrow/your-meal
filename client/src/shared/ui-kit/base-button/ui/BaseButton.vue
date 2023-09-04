ab2fe55b4ecc3947696b02238a2c25ea3e9952c1
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-09-04 15:24:04 +0300

    added base for base-button

---
+++ client/src/shared/ui-kit/base-button/ui/BaseButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="base-button">
+    <div class="base-button__container"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
